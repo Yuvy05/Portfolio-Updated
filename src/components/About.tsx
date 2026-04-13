3f806008d19b4849dcdbd4d424fc02426e303bb4
Yuvy05 <yuvyimmortal@gmail.com>
2026-04-13 16:57:20 +0530

    Perf: Halved TechStack spheres and further condensed About Me bio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,11 +6,10 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am an aspiring Software Engineer and Full-Stack Developer dedicated to the 
-          pursuit of technical excellence. My approach combines rigorous engineering 
-          principles with a passion for user-centric design, allowing me to architect 
-          scalable digital solutions that are both robust and intuitive. Currently 
-          honing my expertise in building high-performance backends using Ruby on Rails.
+          Aspiring Software Engineer and Full-Stack Developer focused on building 
+          scalable, user-centric digital solutions. I combine rigorous engineering 
+          with intuitive design, currently specializing in Ruby on Rails for 
+          high-performance backends.
         </p>
       </div>
     </div>
